c77406514bc3a0383abb6487d3cc9b2eb26feb73
Patrice <pmaxwell30@gatech.edu>
2025-11-22 00:05:14 -0500

    Finish #5

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { Product } from "./models/Product.ts";
 import { apiResponse } from './services/apiService.ts'
 
-const productArray: Promise<Product | null>  =  await apiResponse()
+const productResponse: Promise<Product | null>  =  await apiResponse()
 
+// console.log(productResponse[0])
 
-// console.log(productArray.products)
-for(let x in productArray.products){
-   console.log(productArray.products[x].title)
+const productArray: Product[] = productResponse.map(el => 
+    new Product(el.id, el.title, el.description ,el.category, el.price, el.discountPercentage, el.rating , el.stock, el.sku, el.availabilityStatus, el.reviews, el.images, el.thumbnail)
+)
+
+for(let x in productArray){
+//    console.log(`${productArray[x]?.diplayDetails()} and with tax : $${productArray[x]?.getPriceWithDiscount()}`)
+    console.log(productArray[x]?.diplayDetails())
 }
+
--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,24 +1,23 @@
 import type { Product } from "../models/Product";
-// import { DataError } from "../utils/errorHandler";
-// import { SuperCustomError } from "../utils/errorHandler";
+import { DataError } from "../utils/errorHandler.ts";
+import { SuperCustomError } from "../utils/errorHandler.ts";
 
 export async function apiResponse():Promise<Product | null>{
     
     try {
         const response = await fetch('https://dummyjson.com/products')
-        const productList : Product = await response.json()
-        // console.log(productList)
+        let productList : Product = await response.json()
+        productList = productList.products
         return productList
     
     } catch (error: unknown) {
         if (error instanceof Error) {
             console.error("Caught an Error object:", error.message);
         } else {
-            console.error("Caught an unknown error:" , error)
-            // throw new SuperCustomError("You pyscho, what did YOU DO???" )
-            // throw new DataError("nnsns" , 783)
-    }
-    return null
+            console.error(error)
+            throw new SuperCustomError("You pyscho, what did YOU DO???")
+        }
+        return null
     }
 }
 
